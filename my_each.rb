cb150b71dcb3dea47eaacf2eed00935652e8e9f5
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-05-10 22:57:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,10 @@ def my_each(array)
 end
 
 my_each(array) do
-  i = 0
-  while i < array.length
-  puts array[i]
-  i+=1
-  array
-end
+    i = 0
+    while i < array.length
+      puts array[i]
+      i+=1
+      end
+    array
 end
